3fb5bde77d8b91a8fbd04f23c98139fab15e351e
Daniel . <sngelaneagu5620@gmail.com>
2025-01-22 15:15:18 +0100

    test

--- PythonTest/PythonTest.py
+++ PythonTest/PythonTest.py
@@ -1,22 +1,18 @@
-from time import sleep
+# Line
 
-def fun1():
-    a = 2
-    global b
-    while (a / 5 != 200):
-        print("current number:", a)
-        sleep(.001)
-        b = a = (a + 1)
-    print("final number:", a)
+'''
+Multi-Line
+'''
 
-def fun2():
-    global b
-    while (b > 50):
-        print("current number:", b)
-        sleep(.001)
-        b = b - 1
-    print("final number:", b)
+CamelCase = 1
 
-fun1()
-sleep(3)
-fun2()
+a = "abc"
+b = 1
+c = 1.25
+d = True
+
+x: float = 0.256
+y: bool = False
+z: str = 'Hello, World!'
+
+print(str(c))
